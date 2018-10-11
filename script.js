ef48fe343c5fad9df1a936460478528cec5ceeaf
Jatinder Singh <jsbirdi0001@gmail.com>
2018-10-11 02:14:16 -0400

    JJ

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ $(".readMore").on("click",function(){
 		$(this).html("Read More");
 	}
 })
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+$(".menubar a").click(function(){
+	$("#mySidenav").css("width","0px");
+})
+
+
+
+// Loading JavaScript
+// create the loading sign
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}
